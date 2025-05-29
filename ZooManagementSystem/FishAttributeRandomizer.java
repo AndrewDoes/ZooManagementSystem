0aee1df653470a3bbb3c1594e587b5aa8511371a
Andrew Sutiono <sutionoandrew@gmail.com>
2025-05-30 02:49:01 +0700

    added new architecture for the database and its service

---
+++ ZooManagementSystem/FishAttributeRandomizer.java
@@ -0,0 +1,42 @@
+package ZooManagementSystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class FishAttributeRandomizer{
+public static List<Integer> generateUniqueRandomList(int length) {
+    Set<Integer> uniqueNumbers = new HashSet<>();
+    Random random = new Random();
+
+    while (uniqueNumbers.size() < length) {
+        uniqueNumbers.add(random.nextInt(10));
+    }
+
+    return new ArrayList<>(uniqueNumbers);
+}
+
+public static List<Colour> randomColour() {
+    List<Colour> allColours = Arrays.asList(Colour.values());
+    List<Colour> singularFishColours = new ArrayList<>();
+    int length = (int) (Math.random() * 10) + 1;
+    List<Integer> randomUniqueNumberList = generateUniqueRandomList(length);
+
+    for (Integer index : randomUniqueNumberList) {
+        singularFishColours.add(allColours.get(index));
+        Zoo.numOfFishColours[index]++;
+    }
+
+    return singularFishColours;
+}
+
+	//Returns a random enum value of pattern.
+	public static Pattern randomPattern() {
+		List<Pattern> allPatterns = Arrays.asList(Pattern.values());
+		Random random = new Random();
+		return allPatterns.get(random.nextInt(allPatterns.size()));
+	}
+}
---
+++ ZooManagementSystem/FishService.java
@@ -0,0 +1,48 @@
+package ZooManagementSystem;
+
+import java.util.List;
+
+public class FishService {
+    private AnimalRepository repo;
+
+    public FishService(AnimalRepository repo){
+        this.repo = repo;
+    };
+    
+    public void AddFish(int type, int age, double length, List<Colour> singularFishColours,Pattern Pattern){
+		Fish new_fish=null;
+        // int age, int lifeSpan, double length, List<Colour> colours, Pattern pattern
+			if(type == 3) {
+				new_fish = new ClownFish(age, length);
+			} else if(type == 2) {
+				new_fish = new GoldFish(age, length);
+			} else if (type == 1) {
+				new_fish = new Fish(age,length, singularFishColours, Pattern);
+			}
+            repo.addAnimal("Fish", new_fish);
+            return;
+	}
+	//Type of Fish 1 = Reg Fish
+	//Type of Fish 2 = GoldFish
+	//Type of Fish 3 = ClownFish
+	public List<Animal> addRandomFish(int num) {
+		for (int i = 0; i < num; i++) {
+				int ageTemp = (int)(Math.random() * 10 + 1);
+				double lengthTemp = Math.random() * 10 + 1;
+				int typeOfFish = (int)(Math.random() * 3 + 1);
+				if(typeOfFish == 2) {
+					repo.addAnimal("Fish", new GoldFish(ageTemp, lengthTemp));
+					continue;
+				}
+				if(typeOfFish == 3) {
+					repo.addAnimal("Fish", new ClownFish(ageTemp, lengthTemp));
+					continue;
+				}
+                Fish fish = new Fish(ageTemp, lengthTemp, FishAttributeRandomizer.randomColour(), FishAttributeRandomizer.randomPattern());
+                repo.addAnimal("Fish", fish);
+			}
+        return repo.getAnimals("Fish");
+    }
+
+
+}
